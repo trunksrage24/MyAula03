5fcd23dba4325027f00a253da982c7125ad82fbd
Vitor Daniel <vitor.daniel@outlook.pt>
2019-03-15 09:46:21 +0000

    EX01 - operações com incrementos e decrementos

--- IncDec/Program.cs
+++ IncDec/Program.cs
@@ -6,7 +6,13 @@ namespace IncDec
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int a = 2, b = -5, c;
+            c = a++ + b++;
+            Console.WriteLine(c);
+
+            int d = 2, e = -5, f;
+            f = --d - --e;
+            Console.WriteLine(f);
         }
     }
 }
