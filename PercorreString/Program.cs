f32bc5abd7ccfd41b334aa6b78367c24cc3aa269
Vitor Daniel <vitor.daniel@outlook.pt>
2019-03-15 12:41:13 +0000

    EX04 - percorre string

---
+++ PercorreString/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace PercorreString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //declare variables
+            string s, cStr;
+            char c;
+            //ask for a string
+            Console.WriteLine("insert string");
+            s = Console.ReadLine();
+            //ask for a  char
+            Console.WriteLine("insert char");
+            cStr = Console.ReadLine();
+            //convert string to char
+            c = cStr[0];
+            //print string
+            //run string
+            foreach(char cInString in s)
+            {
+                //current char != user char?
+                if (cInString != c)
+                {
+                    //print current char from string
+                    Console.Write(cInString);
+                }
+            }
+
+
+        }
+    }
+}
